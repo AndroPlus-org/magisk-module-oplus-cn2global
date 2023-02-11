c660a3c34045aa1e427f6aa2163620282bb48292
AndroPlus-org <hosigumayuugi@gmail.com>
2023-02-11 11:05:32 +0900

    Fix

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -24,9 +24,9 @@ maybe_set_prop gsm.sim.operator.numeric "," "44011,44011"
 maybe_set_prop gsm.sim.operator.iso-country "," "jp,jp"
 
 # Disable CN GMS restriction
-mount -o ro,bind $MODDIR/permissions/oplus.feature.control_cn_gms.xml /my_bigball/etc/permissions/oplus.feature.control_cn_gms.xml
-mount -o ro,bind $MODDIR/permissions/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml
-mount -o ro,bind $MODDIR/permissions/oplusfeature.region_cn.com.oplus.battery.xml /my_bigball/etc/permissions/oplusfeature.region_cn.com.oplus.battery.xml
+mount -o ro,bind $MODDIR/xml/permissions/oplus.feature.control_cn_gms.xml /my_bigball/etc/permissions/oplus.feature.control_cn_gms.xml
+mount -o ro,bind $MODDIR/xml/permissions/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml
+mount -o ro,bind $MODDIR/xml/permissions/oplusfeature.region_cn.com.oplus.battery.xml /my_bigball/etc/permissions/oplusfeature.region_cn.com.oplus.battery.xml
 
-mount -o ro,bind $MODDIR/permissions/EuiccGoogle_grant_permissions_list.xml /my_bigball/etc/permissions/EuiccGoogle_grant_permissions_list.xml
-mount -o ro,bind $MODDIR/EuiccGoogle /my_bigball/priv-app/EuiccGoogle
+mount -o ro,bind $MODDIR/xml/permissions/EuiccGoogle_grant_permissions_list.xml /my_bigball/etc/permissions/EuiccGoogle_grant_permissions_list.xml
+mount -o ro,bind $MODDIR/xml/EuiccGoogle /my_bigball/priv-app/EuiccGoogle
