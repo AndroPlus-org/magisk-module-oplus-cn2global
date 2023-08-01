13c39df3afe7328f71fd89620b45e22c95b2b22c
AndroPlus-org <hosigumayuugi@gmail.com>
2023-08-01 20:11:28 +0900

    Move region unlock commands to post-fs-data

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -90,4 +90,17 @@ if [ -d ${MODDIR}/xml/overlay/GmsConfigOverlayComms.apk ];then
 mount -o ro,bind $MODDIR/xml/overlay/GmsConfigOverlayComms.apk /my_heytap/overlay/GmsConfigOverlayComms.apk
 fi
 
-resetprop persist.sys.oplus.radio.globalregionlock 0,0
+(
+while true
+do
+    setprop persist.sys.oplus.radio.globalregionlock 0,0
+	setprop persist.sys.oplus.radio.haslimited false
+	setprop ro.oplus.radio.checkservice false
+	setprop persist.sys.oplus.bnoticetimes -200000
+	setprop persist.sys.oplus.pnoticetimes -200000
+	setprop gsm.sim.oplus.radio.fnoticetime -200000
+
+    # make this script sleep for 6 hours
+    sleep 6h
+done
+) &
